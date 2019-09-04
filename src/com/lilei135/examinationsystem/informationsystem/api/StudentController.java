200c1a33c87797ce5dca3de079609f4cefd08d59
wangsiqian <365460912@qq.com>
2019-09-04 15:43:36 +0800

    添加学生CRUD

---
+++ src/com/lilei135/examinationsystem/informationsystem/api/StudentController.java
@@ -0,0 +1,116 @@
+package com.lilei135.examinationsystem.informationsystem.api;
+
+import com.lilei135.examinationsystem.informationsystem.models.Student;
+import com.lilei135.examinationsystem.informationsystem.models.Class;
+import org.apache.ibatis.exceptions.PersistenceException;
+import org.apache.ibatis.session.SqlSession;
+import resource.servletapi.BaseHttpServlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
+
+/** @author wangsiqian */
+@WebServlet("/api/v1/student")
+public class StudentController extends BaseHttpServlet {
+    @Override
+    protected byte[] handleGet(HttpServletRequest request) throws UnsupportedEncodingException {
+        SqlSession session = getSession();
+        Student student = session.selectOne("getStudent", request.getParameter("studentId"));
+        if (student == null) {
+            return falseResponse("没有这个学生");
+        } else {
+            return okResponse(student);
+        }
+    }
+
+    @Override
+    protected byte[] handlePost(HttpServletRequest request)
+            throws UnsupportedEncodingException, IOException {
+        Map<String, Object> jsonResult = getJson(request);
+        String studentId = (String) jsonResult.get("studentId");
+        String classId = (String) jsonResult.get("classId");
+        String studentName = (String) jsonResult.get("studentName");
+        String studentGender = (String) jsonResult.get("studentGender");
+
+        if (studentId == null || classId == null || studentName == null || studentGender == null) {
+            return falseResponse("请输入学生Id, 所属班级, 姓名, 性别");
+        }
+
+        SqlSession session = getSession();
+        Class queryClass = session.selectOne("getClass", classId);
+        if (queryClass == null) {
+            return falseResponse("没有这个班级");
+        }
+
+        Student student =
+                new Student(studentId, queryClass.getClassId(), studentName, studentGender);
+        try {
+            session.insert("addStudent", student);
+        } catch (PersistenceException error) {
+            return falseResponse("添加失败");
+        }
+
+        session.commit();
+        return okResponse("添加成功");
+    }
+
+    @Override
+    protected byte[] handlePut(HttpServletRequest request)
+            throws UnsupportedEncodingException, IOException {
+        Map<String, Object> jsonResult = getJson(request);
+        String studentId = (String) jsonResult.get("studentId");
+
+        SqlSession session = getSession();
+        Student student = session.selectOne("getStudent", studentId);
+        if (student == null) {
+            return falseResponse("没有这个学生");
+        }
+
+        String studentName = (String) jsonResult.get("studentName");
+        if (studentName != null) {
+            student.setStudentName(studentName);
+        }
+
+        String classId = (String) jsonResult.get("classId");
+        if (classId != null) {
+            student.setClassId(Integer.parseInt(classId));
+        }
+
+        String studentGender = (String) jsonResult.get("studentGender");
+        if (studentGender != null) {
+            student.setStudentGender(studentGender);
+        }
+
+        try {
+            session.update("updateStudent", student);
+        } catch (PersistenceException error) {
+            return falseResponse("更新失败");
+        }
+
+        session.commit();
+        return okResponse("更新成功");
+    }
+
+    @Override
+    protected byte[] handleDelete(HttpServletRequest request) throws UnsupportedEncodingException {
+        SqlSession session = getSession();
+        String studentId = request.getParameter("studentId");
+        Student student = session.selectOne("getStudent", studentId);
+
+        if (student == null) {
+            return falseResponse("没有这个学生");
+        }
+
+        try {
+            session.delete("deleteStudent", studentId);
+        } catch (PersistenceException error) {
+            return falseResponse("删除失败");
+        }
+
+        session.commit();
+        return okResponse("删除成功");
+    }
+}
